801388871c5523c2bed6c5be952aa9623acf9940
Arkam-X <charkam786@gmail.com>
2025-12-10 02:54:02 +0530

    Firebase database setup successfully

--- prep_interview_assistant/app/(root)/layout.tsx
+++ prep_interview_assistant/app/(root)/layout.tsx
@@ -1,8 +1,19 @@
 import React, { ReactNode } from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 
 const RootLayout = ({children}: {children: ReactNode}) => {
   return (
-    <div>{children}</div>
+    <div className='root-layout'>
+      <nav>
+        <Link href="/" className='flex items-center gap-2'>
+        <Image src="/logo.svg" alt='logo' width={34} height={30}/>
+        <h2 className='text-primary-100 main-heading'>Prep AI</h2>
+        </Link>
+      </nav>
+
+      {children}
+    </div>
   )
 }
 
