99537bbc17d3cb89e97816650581849616a6f6d4
Arkam-X <charkam786@gmail.com>
2025-12-12 16:17:21 +0530

    Added Firebase folder

---
+++ prep_interview_assistant/firebase/client.ts
@@ -0,0 +1,18 @@
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBmijxqQHsMWLuMSCZCb06TZ9U4zdulfCc",
+  authDomain: "prepai-f5816.firebaseapp.com",
+  projectId: "prepai-f5816",
+  storageBucket: "prepai-f5816.firebasestorage.app",
+  messagingSenderId: "785407085912",
+  appId: "1:785407085912:web:88612d6e0500751c0c292d",
+  measurementId: "G-BB710X29LJ"
+};
+
+const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
